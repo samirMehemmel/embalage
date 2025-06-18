3f1523f2e5f91391155a64a9ed6948655e9640cf
samir mehemmel <mehemmelsamir@gmail.com>
2025-06-19 00:32:39 +0200

    1st comit

---
+++ react/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <header>
+        <div className="header-container">
+            <div className="logo-banner">
+                <img src="" alt="" />
+            </div>
+            <div className="nav_banner">
+                <ul className='navlist'>
+                    <li>Home</li>
+                    <li>A propos</li>
+                    <li>Produits</li>
+                    <li>Obtenir un devis</li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+)
+}
+
+export default Header
